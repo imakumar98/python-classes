15f91ce2cd343150e97709135b7002427f7796b7
Ashwani <imakumar98@gmail.com>
2021-02-01 23:12:31 +0530

    adds assignment

---
+++ 1-feb-21/conditiions.py
@@ -0,0 +1,16 @@
+a = int(input("Enter your class : "))
+
+if(a == 1):
+    print("I study in Class 1st")
+
+elif(a == 2):
+    print("I study in Class 2nd")
+
+elif(a == 3):
+    print("I study in Class 3rd")
+
+else:
+    print("Invalid class value")
+
+
+
